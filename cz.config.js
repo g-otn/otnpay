184d8ab27948848beb6e05d53179a361b1dc5201
g-otn <44736064+g-otn@users.noreply.github.com>
2026-03-01 00:43:50 -0300

    refactor: ♻️ czg config

---
+++ cz.config.js
@@ -0,0 +1,59 @@
+const { definePrompt } = require('czg');
+
+module.exports = definePrompt({
+  useEmoji: true,
+  emojiAlign: 'center',
+  types: [
+    { value: 'feat', name: 'feat:     ✨ A new feature', emoji: '✨' },
+    { value: 'fix', name: 'fix:      🐛 A bug fix', emoji: '🐛' },
+    {
+      value: 'docs',
+      name: 'docs:     📝 Documentation only changes',
+      emoji: '📝',
+    },
+    {
+      value: 'style',
+      name: 'style:    💄 Changes that do not affect the meaning of the code',
+      emoji: '💄',
+    },
+    {
+      value: 'refactor',
+      name: 'refactor: ♻️  A code change that neither fixes a bug nor adds a feature',
+      emoji: '♻️',
+    },
+    {
+      value: 'perf',
+      name: 'perf:     ⚡️ A code change that improves performance',
+      emoji: '⚡️',
+    },
+    {
+      value: 'test',
+      name: 'test:     ✅ Adding missing tests or correcting existing tests',
+      emoji: '✅',
+    },
+    {
+      value: 'build',
+      name: 'build:    📦️ Changes that affect the build system or external dependencies',
+      emoji: '📦️',
+    },
+    {
+      value: 'ci',
+      name: 'ci:       🎡 Changes to our CI configuration files and scripts',
+      emoji: '🎡',
+    },
+    {
+      value: 'chore',
+      name: "chore:    🔧 Other changes that don't modify src or test files",
+      emoji: '🔧',
+    },
+    {
+      value: 'revert',
+      name: 'revert:   ⏪️ Reverts a previous commit',
+      emoji: '⏪️',
+    },
+  ],
+  scopes: [],
+  allowCustomScopes: true,
+  allowEmptyScopes: true,
+  skipQuestions: ['footer'],
+});
