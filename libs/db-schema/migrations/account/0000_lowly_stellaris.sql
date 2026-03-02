09a42c378b52bfc669949c6a4c5645ee87a8759d
g-otn <44736064+g-otn@users.noreply.github.com>
2026-03-02 08:03:17 -0300

    chore(db-schema): 🔧 regenerate schema migrations

--- libs/db-schema/migrations/account/0000_lowly_stellaris.sql
+++ libs/db-schema/migrations/account/0000_lowly_stellaris.sql
@@ -1,19 +1,20 @@
 CREATE TYPE "public"."transaction_type" AS ENUM('deposit', 'withdrawal');--> statement-breakpoint
 CREATE TABLE "accounts" (
-	"id" serial PRIMARY KEY NOT NULL,
 	"account_id" integer,
 	"balance" numeric(18, 2) DEFAULT '0.00' NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
+	"id" serial PRIMARY KEY NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "accounts_account_id_unique" UNIQUE("account_id")
+	CONSTRAINT "accounts_account_id_unique" UNIQUE("account_id"),
+	CONSTRAINT "accounts_balance_non_negative" CHECK ("accounts"."balance" >= 0)
 );
 --> statement-breakpoint
 CREATE TABLE "transactions" (
-	"id" serial PRIMARY KEY NOT NULL,
 	"account_id" integer NOT NULL,
-	"type" "transaction_type" NOT NULL,
 	"amount" numeric(18, 2) NOT NULL,
-	"timestamp" timestamp DEFAULT now() NOT NULL
+	"id" serial PRIMARY KEY NOT NULL,
+	"timestamp" timestamp DEFAULT now() NOT NULL,
+	"type" "transaction_type" NOT NULL
 );
 --> statement-breakpoint
 ALTER TABLE "transactions" ADD CONSTRAINT "transactions_account_id_accounts_account_id_fk" FOREIGN KEY ("account_id") REFERENCES "public"."accounts"("account_id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
--- libs/db-schema/migrations/auth/0000_gigantic_human_torch.sql
+++ libs/db-schema/migrations/auth/0000_gigantic_human_torch.sql
@@ -4,8 +4,7 @@ CREATE TABLE "user" (
 	"email" text NOT NULL,
 	"owner_name" text NOT NULL,
 	"password" text NOT NULL,
-	CONSTRAINT "user_email_unique" UNIQUE("email"),
-	CONSTRAINT "user_owner_name_unique" UNIQUE("owner_name")
+	CONSTRAINT "user_email_unique" UNIQUE("email")
 );
 --> statement-breakpoint
 CREATE INDEX "user_email_idx" ON "user" USING btree ("email");
