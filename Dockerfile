829d12d6ff3f3d85e8465ea8080cf69bc471697d
g-otn <44736064+g-otn@users.noreply.github.com>
2026-03-03 05:31:07 -0300

    feat: ✨ setup account-service in compose, use same Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,16 @@
 # Dockerfile for local development and testing only
+
+# Image with Node.js and Wrangler CLI installed
 FROM jfxs/wrangler
 
-# Copy workspace root manifests for dependency resolution
+# We need the whole monorepo setup, so we can resolve dependencies across workspace packages.
+# This also lets us make changes without having to publish an image,
+# at the cost of being a big image due to all the workspace dependencies
 COPY ./ ./
 
+# Wrangler needs the Node runtime, but we can still use Bun.js as the package manager
 RUN npm i -g bun
 RUN bun install
 
-WORKDIR /data/apps/service-auth
-
-EXPOSE 9010
 
 CMD ["npx", "wrangler", "dev", "--ip", "0.0.0.0", "--env", "dev_docker"]
--- libs/db-schema/migrations/account/0000_goofy_redwing.sql
+++ libs/db-schema/migrations/account/0000_goofy_redwing.sql
@@ -1,24 +1,24 @@
 CREATE TYPE "public"."transaction_type" AS ENUM('deposit', 'withdrawal');--> statement-breakpoint
 CREATE TABLE "accounts" (
-	"account_id" integer,
+	"user_id" integer,
 	"balance" numeric(18, 2) DEFAULT '0.00' NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"id" serial PRIMARY KEY NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "accounts_account_id_unique" UNIQUE("account_id"),
+	CONSTRAINT "accounts_user_id_unique" UNIQUE("user_id"),
 	CONSTRAINT "accounts_balance_non_negative" CHECK ("accounts"."balance" >= 0)
 );
 --> statement-breakpoint
 CREATE TABLE "transactions" (
-	"account_id" integer NOT NULL,
+	"user_id" integer NOT NULL,
 	"amount" numeric(18, 2) NOT NULL,
 	"id" serial PRIMARY KEY NOT NULL,
 	"timestamp" timestamp DEFAULT now() NOT NULL,
 	"type" "transaction_type" NOT NULL
 );
 --> statement-breakpoint
-ALTER TABLE "transactions" ADD CONSTRAINT "transactions_account_id_accounts_account_id_fk" FOREIGN KEY ("account_id") REFERENCES "public"."accounts"("account_id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-CREATE INDEX "accounts_account_id_idx" ON "accounts" USING btree ("account_id");--> statement-breakpoint
-CREATE INDEX "transactions_account_id_idx" ON "transactions" USING btree ("account_id");--> statement-breakpoint
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_user_id_accounts_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."accounts"("user_id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "accounts_user_id_idx" ON "accounts" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "transactions_user_id_idx" ON "transactions" USING btree ("user_id");--> statement-breakpoint
 CREATE INDEX "transactions_type_idx" ON "transactions" USING btree ("type");--> statement-breakpoint
 CREATE INDEX "transactions_timestamp_idx" ON "transactions" USING btree ("timestamp");
